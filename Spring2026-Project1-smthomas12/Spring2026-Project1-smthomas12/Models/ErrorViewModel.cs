eb01375532c02abc7f78d8cff57dd00447f021c3
Sebastian-M-Thomas <sebas81205@yahoo.com>
2026-02-10 18:06:50 -0600

    Add files via upload

---
+++ Spring2026-Project1-smthomas12/Spring2026-Project1-smthomas12/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project1_smthomas12.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Spring2026-Project1-smthomas12/Spring2026-Project1-smthomas12/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿const characters = {
+    luffy: "Monkey D. Luffy - Captain of the Straw Hat pirates.",
+    zoro: "Roranoa Zoro - Swordsman of the Straw Hat pirates and right hand to Monkey D. Luffy.",
+    nami: "Nami - Navigator of the Straw Hat pirates.",
+    usopp: "Usopp - Sniper of the Straw Hat pirates.",
+    sanji: "Vinsmoke Sanji - Cook of the Straw Hat pirates and left hand to Monkey D. Luffy.",
+};
+
+function showCharacter(name) {
+    const infoBox = document.getElementById("characterInfo");
+    infoBox.style.display = "block";
+    infoBox.textContent = characters[name];
+}
